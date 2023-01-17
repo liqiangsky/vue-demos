b94b800b55505a24c5e747009249f08312d63673
李强 <12580liqiang@gmail.com>
2023-01-17 19:44:19 +0800

    add(component): pagination.vue

--- components/pagination/index.vue
+++ components/pagination/index.vue
@@ -1,27 +1,77 @@
 <template>
-  <div class="pagination" v-if="!(hideOnSinglePage && current === 1 && maxPage === 1)">
+  <div
+    class="pagination"
+    v-if="!(hideOnSinglePage && current === 1 && maxPage === 1)"
+  >
     <!-- 左 -->
-    <div v-if="isShow('prev')" class="pagination-item opt" :class="{disabled: current === 1}" @click.stop="handleCurrentChange(getPrevValue, 'prev-click')">
+    <div
+      v-if="isShow('prev')"
+      class="pagination-item opt"
+      :class="{ disabled: current === 1 }"
+      @click.stop="handleCurrentChange(getPrevValue, 'prev-click')"
+    >
       <template v-if="prevText">{{ prevText }}</template>
       <template v-else>
         <i class="el-icon-arrow-left"></i>
       </template>
     </div>
     <template v-if="isShow('pager')">
-      <div class="pagination-item" :class="{active: current === 1}" @click.stop="handleCurrentChange(1)">1</div>
+      <div
+        class="pagination-item"
+        :class="{ active: current === 1 }"
+        @click.stop="handleCurrentChange(1)"
+      >
+        1
+      </div>
       <!-- 左前 -->
-      <div class="pagination-item" v-if="renderPaginations[0] - 1 > 1" @mouseenter.stop="mouseenter('left')" @mouseleave.stop="mouseleave" @click.stop="handleCurrentChange(getLeftValue)">
-        <i :class="mouse === 'left' ? 'el-icon-d-arrow-left' : 'el-icon-more'"></i>
+      <div
+        class="pagination-item"
+        v-if="renderPaginations[0] - 1 > 1"
+        @mouseenter.stop="mouseenter('left')"
+        @mouseleave.stop="mouseleave"
+        @click.stop="handleCurrentChange(getLeftValue)"
+      >
+        <i
+          :class="mouse === 'left' ? 'el-icon-d-arrow-left' : 'el-icon-more'"
+        ></i>
       </div>
-      <div class="pagination-item" :class="{active: current === item}" v-for="item in renderPaginations" :key="item" @click.stop="handleCurrentChange(item)">{{ item }}</div>
+      <div
+        class="pagination-item"
+        :class="{ active: current === item }"
+        v-for="item in renderPaginations"
+        :key="item"
+        @click.stop="handleCurrentChange(item)"
+      >
+        {{ item }}
+      </div>
       <!-- 右前 -->
-      <div class="pagination-item" v-if="maxPage - renderPaginations[renderPaginations.length - 1] > 1" @mouseenter.stop="mouseenter('right')" @mouseleave.stop="mouseleave" @click.stop="handleCurrentChange(getRightValue)">
-        <i :class="mouse === 'right' ? 'el-icon-d-arrow-right' : 'el-icon-more'"></i>
+      <div
+        class="pagination-item"
+        v-if="maxPage - renderPaginations[renderPaginations.length - 1] > 1"
+        @mouseenter.stop="mouseenter('right')"
+        @mouseleave.stop="mouseleave"
+        @click.stop="handleCurrentChange(getRightValue)"
+      >
+        <i
+          :class="mouse === 'right' ? 'el-icon-d-arrow-right' : 'el-icon-more'"
+        ></i>
       </div>
-      <div class="pagination-item" v-if="maxPage !== 1" :class="{active: current === maxPage}" @click.stop="handleCurrentChange(maxPage)">{{ maxPage }}</div>
+      <div
+        class="pagination-item"
+        v-if="maxPage !== 1"
+        :class="{ active: current === maxPage }"
+        @click.stop="handleCurrentChange(maxPage)"
+      >
+        {{ maxPage }}
+      </div>
     </template>
     <!-- 右 -->
-    <div  v-if="isShow('next')" class="pagination-item opt" :class="{disabled: current === maxPage}" @click.stop="handleCurrentChange(getNextValue, 'next-click')">
+    <div
+      v-if="isShow('next')"
+      class="pagination-item opt"
+      :class="{ disabled: current === maxPage }"
+      @click.stop="handleCurrentChange(getNextValue, 'next-click')"
+    >
       <template v-if="nextText">{{ nextText }}</template>
       <template v-else>
         <i class="el-icon-arrow-right"></i>
@@ -31,11 +81,11 @@
 </template>
 <script>
 export default {
-  name: "Pagination",
+  name: "LPagination",
   props: {
     value: {
       type: Number,
-      default: 1
+      default: 1,
     },
     total: {
       type: Number,
@@ -47,59 +97,59 @@ export default {
     },
     pagerCount: {
       type: Number,
-      default: 7
+      default: 7,
     },
     layout: {
       type: String,
-      default: 'prev,pager,next'
+      default: "prev,pager,next",
     },
     prevText: {
       type: String,
-      default: ''
+      default: "",
     },
     nextText: {
       type: String,
-      default: ''
+      default: "",
     },
     hideOnSinglePage: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
   computed: {
-    Layout () {
-      return this.layout.split(',');
+    Layout() {
+      return this.layout.split(",");
     },
     // 中间区展示的个数
-    pagerCountCenter () {
+    pagerCountCenter() {
       return this.pagercount - 2;
     },
     // 最大页码
-    maxPage () {
+    maxPage() {
       return Math.ceil(this.total / this.pageSize) || 1;
     },
     // 前
-    getPrevValue () {
+    getPrevValue() {
       const currentPage = this.current - 1;
       return currentPage < 1 ? 1 : currentPage;
     },
     // 后
-    getNextValue () {
+    getNextValue() {
       const currentPage = this.current + 1;
       return currentPage > this.maxPage ? this.maxPage : currentPage;
     },
     // 左近
-    getLeftValue () {
+    getLeftValue() {
       const currentPage = this.current - this.pagerCountCenter;
       return currentPage < 1 ? 1 : currentPage;
     },
     // 右进
-    getRightValue () {
+    getRightValue() {
       const currentPage = this.current + this.pagerCountCenter;
       return currentPage > this.maxPage ? this.maxPage : currentPage;
     },
     // 中间区渲染
-    renderPaginations () {
+    renderPaginations() {
       // 如果中间的个数小于展示的个数那么全部展示 否则按pagerCountCenter展示
       if (this.paginations.length <= this.pagerCountCenter) {
         return this.paginations;
@@ -116,50 +166,52 @@ export default {
       // 开始的最大索引
       const max = this.paginations.length - this.pagerCountCenter;
       // 开始索引
-      const start = index - num <= 0 ? 0 : index - num >= max ? max : index - num;
+      const start =
+        index - num <= 0 ? 0 : index - num >= max ? max : index - num;
       // 结束
       const end = start + this.pagerCountCenter;
       return this.paginations.slice(start, end);
-    }
+    },
   },
   watch: {
-    value (newVal) {
+    value(newVal) {
       this.current = newVal > this.maxPage ? this.maxPage : newVal;
     },
-    current (newVal) {
-      this.$emit('input', newVal);
-      this.$emit('current-change', newVal);
+    current(newVal) {
+      this.$emit("input", newVal);
+      this.$emit("current-change", newVal);
     },
     pagerCount: {
-      handler (newVal) {
-        this.pagercount = newVal >= 5 && newVal <= 21 && newVal % 2 !== 0 ? newVal : 7;
+      handler(newVal) {
+        this.pagercount =
+          newVal >= 5 && newVal <= 21 && newVal % 2 !== 0 ? newVal : 7;
         this.init();
       },
-      immediate: true
+      immediate: true,
     },
-    pageSize () {
+    pageSize() {
       this.init();
     },
     total: {
-      handler () {
+      handler() {
         this.init();
       },
-      immediate: true
-    }
+      immediate: true,
+    },
   },
-  data () {
+  data() {
     return {
       pagercount: this.pagerCount,
-      mouse: '',
+      mouse: "",
       current: this.value,
-      paginations: []
+      paginations: [],
     };
   },
   methods: {
-    isShow (value) {
-      return this.Layout.some(item => item === value);
+    isShow(value) {
+      return this.Layout.some((item) => item === value);
     },
-    init () {
+    init() {
       const current = this.current;
       this.current = 1;
       this.paginations = [];
@@ -168,17 +220,17 @@ export default {
       }
       this.current = current > this.maxPage ? this.maxPage : current;
     },
-    handleCurrentChange (v, eventName) {
+    handleCurrentChange(v, eventName) {
       this.current = v;
       eventName && this.$emit(eventName, v);
     },
-    mouseenter (v) {
-      this.mouse = v;      
+    mouseenter(v) {
+      this.mouse = v;
     },
-    mouseleave () {
-      this.mouse = '';
-    }
-  }
+    mouseleave() {
+      this.mouse = "";
+    },
+  },
 };
 </script>
 <style scoped>
