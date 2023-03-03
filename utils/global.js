0041503f01b35ee900be73de1ee26d8e70d0ed2a
李强 <12580liqiang@gmail.com>
2023-03-03 14:21:33 +0800

    add(util): global.js

---
+++ utils/global.js
@@ -0,0 +1,6 @@
+const devicePixelRatio = () => window.devicePixelRatio;
+const userAgent = () => window.navigator.userAgent;
+const platform = () => window.navigator.platform;
+const maxTouchPoints = () => window.navigator.maxTouchPoints;
+
+export { devicePixelRatio, userAgent, platform, maxTouchPoints };
