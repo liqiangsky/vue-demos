d722b22f3bd63723667da184d1c20777195f77a2
李强 <12580liqiang@gmail.com>
2023-03-09 10:43:29 +0800

    update(util): global.js requestAnimationFrame、cancelAnimationFrame

--- utils/global.js
+++ utils/global.js
@@ -2,5 +2,17 @@ const devicePixelRatio = () => window.devicePixelRatio;
 const userAgent = () => window.navigator.userAgent;
 const platform = () => window.navigator.platform;
 const maxTouchPoints = () => window.navigator.maxTouchPoints;
+const requestAnimationFrame = (callback) => {
+  const fn =
+    window.requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
+  return fn(callback);
+};
+const cancelAnimationFrame = (requestID) => {
+  const fn = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+  fn(requestID);
+};
 
-export { devicePixelRatio, userAgent, platform, maxTouchPoints };
+export { devicePixelRatio, userAgent, platform, maxTouchPoints, requestAnimationFrame, cancelAnimationFrame };
